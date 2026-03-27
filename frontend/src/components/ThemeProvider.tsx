ad81e611660c8f0736ba91e5703dcbb87accef7e
Dydex <supremeajala@gmail.com>
2026-03-27 07:39:24 +0100

    Fix: changed defaultTheme to system

--- frontend/src/components/ThemeProvider.tsx
+++ frontend/src/components/ThemeProvider.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 
 export default function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </NextThemesProvider>
   );
